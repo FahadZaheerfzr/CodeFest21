95d39e023b74cc834f122f9d2e5ebe447da36968
Fahad Zaheer <52493343+FahadZaheerfzr@users.noreply.github.com>
2021-12-25 21:33:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { WebhookClient } = require("dialogflow-fulfillment");
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.send("I have listened");
+});
+app.post("/dialogflow", express.json(), (req, res) => {
+    const agent = new WebhookClient({ request: req, response: res });
+    let intentMap = new Map();
+    intentMap.set("Default Welcome Intent", help);
+    intentMap.set("Default Fallback Intent", defaultFallback);
+    agent.handleRequest(intentMap);
+});
+
+function welcome(agent) {
+    agent.add('Hi, I am assistant. I can help you in various service. How can I help you today?');
+}
+
+
+function defaultFallback(agent) {
+    agent.add('Sorry! I am unable to understand this at the moment. I am still learning humans. You can pick any of the service that might help me.');
+}
+app.listen(process.env.PORT || 8080,()=>{
+    console.log("Listening on port 8080");
+});
